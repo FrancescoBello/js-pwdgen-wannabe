579909a085ffdf99179565b06d4423716875e598
Francesco <fraancesco.bello@gmail.com>
2020-10-12 18:30:48 +0200

    finish

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+var nome_utente = prompt('Inserisci qui il tuo nome');
+
+//document.writeln(nome_utente);
+
+var cognome_utente = prompt('Inserisci qui il tuo cognome');
+
+//document.writeln(cognome_utente);
+
+var colore_preferito = prompt('quale è il tuo colore preferito?');
+
+document.writeln(nome_utente + cognome_utente + colore_preferito + 20);
